3d4e8b0006c24602039ed1d70829d0a81fb0240a
Naveen Krishnan <naveenkrishnan01@gmail.com>
2015-05-21 21:15:33 -0700

    modify redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,3 +1,2 @@
-if ENV["REDISCLOUD_URL"]
-    $redis = Redis.new(:url => ENV["REDISCLOUD_URL"])
-end
+uri = URI.parse(ENV["REDISTOGO_URL"] || "redis://localhost:6379/" )
+REDIS = Redis.new(:host => uri.host, :port => uri.port, :password => uri.password)
